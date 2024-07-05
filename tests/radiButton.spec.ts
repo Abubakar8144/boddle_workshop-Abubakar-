fa2347abf65459df0f2d77ee46640b71dc30fa05
Abubakar8144 <140054289+Abubakar8144@users.noreply.github.com>
2024-07-05 19:39:31 +0500

    Handling Radio Buttons
    In first test case(radio buttons workshop), I have implemented the task during our workshop by my-self.
    
    In second test case(radio buttons), I have implemented the task by following the steps which were covered in the workshop

---
+++ tests/radiButton.spec.ts
@@ -0,0 +1,59 @@
+import { test, expect } from '@playwright/test';
+
+test("radio buttons workshop", async({page})=>{
+    
+    await page.goto("https://demoqa.com/")
+
+    await page.locator(".card-body").getByText("Elements").click()
+    await page.locator(".text").getByText("Radio Button").click()
+    
+    await page.waitForLoadState("domcontentloaded")
+    await expect(await page.locator(".text-center")).toBeVisible()
+    await expect(page.url()).toContain('https://demoqa.com/radio-button')
+
+    await page.locator("#yesRadio").check({force:true})
+    await expect(await page.locator(".mt-3")).toHaveText("You have selected Yes")
+
+    await page.locator("#impressiveRadio").check({force:true})
+    await expect(await page.locator(".mt-3")).toHaveText("You have selected Impressive")
+    
+    //await page.locator("#noRadio").check({force:true})
+    
+    // const checked = await page.locator("#noRadio").isChecked()
+    // await expect(checked).toBeTruthy()
+
+})
+
+test("radio buttons", async({page})=>{
+
+    await page.goto("/")
+
+    await page.locator(".card-body").getByText("Elements").click()
+    await page.locator(".text").getByText("Radio Button").click()
+    
+    await page.waitForLoadState("domcontentloaded")
+    await expect(await page.locator(".text-center")).toBeVisible()
+    await expect(page.url()).toContain('https://demoqa.com/radio-button')
+
+    async function selectRadioButtonDynamically(index) {
+        const locator = `//div[@class="custom-control custom-radio custom-control-inline"][${index}]`
+        const label = await page.locator(`${locator}/label`).innerText()
+
+        await page.locator(`${locator}/input`).check({force:true})
+
+        const isChecked = await page.locator(`${locator}/input`).isChecked()
+
+        expect(isChecked).toBeTruthy()
+
+        return label
+   }
+
+   const labelValue1 = await selectRadioButtonDynamically("1")
+
+   await expect(await page.locator("p.mt-3")).toHaveText(`You have selected `+labelValue1)
+
+   const labelValue2 = await selectRadioButtonDynamically("2")
+
+   await expect(await page.locator("p.mt-3")).toHaveText(`You have selected `+labelValue2)
+   
+})
